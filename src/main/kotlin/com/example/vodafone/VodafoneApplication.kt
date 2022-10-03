fe17d8b6bfdd3e055bdc45e871094f8266932c69
Berkant Erbey <erbeyberkant@gmail.com>
2022-10-03 18:33:11 +0300

    initial commit

---
+++ src/main/kotlin/com/example/vodafone/VodafoneApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vodafone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VodafoneApplication
+
+fun main(args: Array<String>) {
+	runApplication<VodafoneApplication>(*args)
+}
